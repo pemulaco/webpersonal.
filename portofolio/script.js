4c97f426118e1cbeaa41541fabbb46b72a4f74bf
pemulaco <154732483+pemulaco@users.noreply.github.com>
2024-03-16 02:29:37 +0700

    Add files via upload

---
+++ portofolio/script.js
@@ -0,0 +1,18 @@
+// responsive navigation 
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+navSlide();
+// clear before unload 
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset()
+    }
+}
+
